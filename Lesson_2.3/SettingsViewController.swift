6e40438a51da05be596572aa79e8bdfe5ba6bc16
enjoy2k <Enjoy2k@mail.ru>
2022-10-03 17:05:01 +0300

    Приложение работоспособно

--- Lesson_2.3/SettingsViewController.swift
+++ Lesson_2.3/SettingsViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    @IBOutlet var minValueTF: UITextField! // TF - Text Field
+    @IBOutlet var maxValueTF: UITextField!
+    
+    var minValue: String! // Восклицательный знак означает что я не должен обращаться к свойству, пока оно не инициализировано
+    var maxValue: String! // Инициализация этих свойств возможна после того как супер Вью загрузится в память(СуперВью это основной экран)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-   
+        minValueTF.text = minValue
+        maxValueTF.text = maxValue // После того как я инициализировал публичные свойства на предыдущей странице, я могу обращаться к аутлетам этого класса
     }
 
+    @IBAction func cancelButtonPressed() {
+        dismiss(animated: true) // Здесь икскод предлагает указать замыкание. В данном случае не нужно, но может понадобиться, если требуется выполнить какой-то код по закрытию
+    }
 }
--- Lesson_2.3/ViewController.swift
+++ Lesson_2.3/ViewController.swift
@@ -15,17 +15,29 @@ class ViewController: UIViewController {
     @IBOutlet var minValueLabel: UILabel!
     @IBOutlet var maxValueLabel: UILabel!
     @IBOutlet var randomValueLabel: UILabel!
-    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         getResultButton.layer.cornerRadius = 10
-        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return } // Кастим UIViewController до SettingsViewController
+//        Создал экземпляр класса SettingsViewController, и в нём указал данные для отображения на странице настроек при загрузке вью
+        // guard использую чтобы экземпляр класса перестал быть опциональным
+        settingsVC.maxValue = maxValueLabel.text
+        settingsVC.minValue = minValueLabel.text
     }
     @IBAction func getResult() {
         let minNumber = Int(minValueLabel.text ?? "") ?? 0
         let maxNumber = Int(maxValueLabel.text ?? "") ?? 100
         
         randomValueLabel.text = String(Int.random(in: minNumber...maxNumber))
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let settingsVC = segue.source as? SettingsViewController else { return }
+        minValueLabel.text = settingsVC.minValueTF.text
+        maxValueLabel.text = settingsVC.maxValueTF.text
     }
 }
 
